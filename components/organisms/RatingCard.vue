b5b2635b886f2b689b564f271fe9de5bb515b358
Frankz <francisco@getglued.co>
2025-01-06 15:14:55 -0600

    Images fix

--- components/organisms/RatingCard.vue
+++ components/organisms/RatingCard.vue
@@ -1,6 +1,6 @@
 <template lang="html">
     <article class='flex flex-col p-6 rounded-[10px] gap-y-4 text-center items-center justify-center md:min-w-[384px] min-w-[90%] bg-white-default text-blue-dark'>
-        <img :src="getImageUrl(user.picture)" class='w-14 h-14 rounded-full' :alt="user.user_name" />
+        <img :src="user.picture" class='w-14 h-14 rounded-full' :alt="user.user_name" />
         <p class='grow text-[16px] font-[700]'>"{{ user.testimonial }}"</p>
         <div class='text-grey-default flex flex-col gap-0.5'>
             <span class='text-[14px] font-[700]'>{{ user.user_name }}</span>
@@ -12,8 +12,4 @@
 
 <script setup>
     defineProps(['user'])
-
-    function getImageUrl(name) {
-        return new URL(`../../assets/img/clients/${name}`, import.meta.url).href
-    }
 </script>
