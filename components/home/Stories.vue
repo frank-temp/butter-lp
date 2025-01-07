236c1cd72d49d088c3e87936ca367f2f9b64839a
Frankz <francisco@getglued.co>
2025-01-07 15:43:15 -0600

    Stories fix carousel

--- components/home/Stories.vue
+++ components/home/Stories.vue
@@ -5,12 +5,52 @@
             <p>Leading companies use ButterCMS to smoothly create, manage, and scale web, eCommerce, and mobile content.</p>
         </article>
         <article class='w-full overflow-hidden max-w-full md:max-w-[1440px]'>
-            <article class='mt-8 flex flex-row gap-x-6 justify-start pl-4 md:pl-32 pr-4 md:pr-32 flex-nowrap w-full overflow-x-scroll no-scrollbar'>
+            <!-- <article class='mt-8 flex flex-row gap-x-6 justify-start pl-4 md:pl-32 pr-4 md:pr-32 flex-nowrap w-full overflow-x-scroll no-scrollbar'>
                 <story-card v-for="item in data" :id="item.id" :story="item" :key="item.id" />
+            </article> -->
+            <article class='w-full flex items-center justify-center mt-8'>
+                <Carousel v-bind="config" class='max-w-full md:max-w-[1200px] flex flex-row'>
+                    <Slide v-for="slide in data">
+                        <story-card :id="slide.id" :story="slide" :key="slide.id" />
+                    </Slide>
+                    <template #addons>
+                        <Navigation />
+                    </template>
+                </Carousel>
             </article>
         </article>
     </section>
 </template>
+
+<script setup>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
+// Carousel configuration
+const config = {
+  itemsToShow: 3,
+  snapAlign: 'start',
+  breakpointMode: 'carousel',
+  breakpoints: {
+    // 300px and up
+    300: {
+      itemsToShow: 1,
+      snapAlign: 'start',
+    },
+    // 400px and up
+    400: {
+      itemsToShow: 2,
+      snapAlign: 'start',
+    },
+    // 500px and up
+    500: {
+      itemsToShow: 3,
+      snapAlign: 'start',
+    },
+  },
+};
+
+</script>
 <script>
 import StoryCard from "~/components/organisms/StoryCard.vue"
 import stories from "~/assets/data/stories.json"
--- components/organisms/StoryCard.vue
+++ components/organisms/StoryCard.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <article v-if="(story.type === 'left')" class='rounded-[10px] p-6 bg-gradient-blue-lg min-w-[90%] md:min-w-[450px] flex flex-col gap-4 items-start justify-start'>
+    <article v-if="(story.type === 'left')" class='rounded-[10px] p-6 bg-gradient-blue-lg  flex flex-col gap-4 items-start justify-start mx-3 h-full'>
         <div class='flex flex-col items-start justify-between gap-4'>
             <div class='flex flex-row gap-4 items-center justify-center text-blue-dark'>
                 <img class='w-10 h-10 rounded-full' :src="story.picture" :alt="story.user_name" />
@@ -15,10 +15,10 @@
         </div>
         <img class='h-[30px]' :src="story.logo" :alt="story.user_name" />
     </article>
-    <article v-else class='relative bg-gradient-blue-linear p-6 rounded-[10px] min-w-[335px] text-white-default text-center flex flex-col gap-4 items-center justify-center'>
-        <p v-if="(story.user_name === 'Todd Dunning')" class='text-[26px] font-[700] z-[2] grow'>"{{ story.testimonial }}"</p>
-        <p v-else-if="(story.user_name === 'Riley Venable')" class='text-[18px] font-[700] z-[2] grow'>"{{ story.testimonial }}"</p>
-        <p v-else="(story.user_name === 'Jake Kring')" class='text-[20px] font-[700] z-[2] grow'>"{{ story.testimonial }}"</p>
+    <article v-else class='relative bg-gradient-blue-linear p-6 rounded-[10px] text-white-default text-center flex flex-col gap-4 items-center justify-center mx-3 h-full'>
+        <p v-if="(story.user_name === 'Todd Dunning')" class='text-[26px] font-[700] z-[2]'>"{{ story.testimonial }}"</p>
+        <p v-else-if="(story.user_name === 'Riley Venable')" class='text-[18px] font-[700] z-[2]'>"{{ story.testimonial }}"</p>
+        <p v-else="(story.user_name === 'Jake Kring')" class='text-[20px] font-[700] z-[2]'>"{{ story.testimonial }}"</p>
         <div class='flex flex-col items-center justify-center z-[2] text-[14px]'>
             <span class='font-[700]'>{{ story.user_name }}</span>
             <span class='font-[400]'>{{ story.position }}</span>
