c34d3a5eb855f14d235a39df7d656b29df788705
Frankz <francisco@getglued.co>
2025-01-07 13:34:38 -0600

    fonts sizes fix

--- components/organisms/StoryCard.vue
+++ components/organisms/StoryCard.vue
@@ -1,20 +1,24 @@
 <template lang="html">
     <article v-if="(story.type === 'left')" class='rounded-[10px] p-6 bg-gradient-blue-lg min-w-[90%] md:min-w-[450px] flex flex-col gap-4 items-start justify-start'>
-        <div class='flex flex-col items-start justify-start gap-4'>
-            <div class='flex flex-row gap-4 items-center justify-center'>
+        <div class='flex flex-col items-start justify-between gap-4'>
+            <div class='flex flex-row gap-4 items-center justify-center text-blue-dark'>
                 <img class='w-10 h-10 rounded-full' :src="story.picture" :alt="story.user_name" />
-                <div class='flex flex-col items-start justify-start text-blue-dark text-[14px]'>
-                    <span class='font-[600]'>{{ story.user_name }}</span>
-                    <span class='font-[400]'>{{ story.position }}</span>
+                <div class='flex flex-col items-start justify-start text-blue-dark'>
+                    <span class='text-[14px] font-[600]'>{{ story.user_name }}</span>
+                    <span class='text-[14px] font-[400]'>{{ story.position }}</span>
                 </div>
             </div>
-            <p :class="`${story.font_size}`" class='font-[700]'>"{{ story.testimonial }}"</p>
+            <p v-if="(story.user_name === 'Justin Ludlow')" class='text-[34px] font-[700] grow'>"{{ story.testimonial }}"</p>
+            <p v-else-if="(story.user_name === 'Jamey Meeker')" class='text-[18px] font-[700] grow'>"{{ story.testimonial }}"</p>
+            <p v-else class='text-[20px] font-[700] grow'>"{{ story.testimonial }}"</p>
             <p class='text-[14px] font-[400]'>{{ story.sub }}</p>
         </div>
         <img class='h-[30px]' :src="story.logo" :alt="story.user_name" />
     </article>
     <article v-else class='relative bg-gradient-blue-linear p-6 rounded-[10px] min-w-[335px] text-white-default text-center flex flex-col gap-4 items-center justify-center'>
-        <p :class="`${story.font_size}`" class='font-[700] z-[2]'>"{{ story.testimonial }}"</p>
+        <p v-if="(story.user_name === 'Todd Dunning')" class='text-[26px] font-[700] z-[2] grow'>"{{ story.testimonial }}"</p>
+        <p v-else-if="(story.user_name === 'Riley Venable')" class='text-[18px] font-[700] z-[2] grow'>"{{ story.testimonial }}"</p>
+        <p v-else="(story.user_name === 'Jake Kring')" class='text-[20px] font-[700] z-[2] grow'>"{{ story.testimonial }}"</p>
         <div class='flex flex-col items-center justify-center z-[2] text-[14px]'>
             <span class='font-[700]'>{{ story.user_name }}</span>
             <span class='font-[400]'>{{ story.position }}</span>
